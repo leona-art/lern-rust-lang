9f517a32964fc5f6628319ed2906337ffaf60da0
leona-art <leonagraph@gmail.com>
2025-03-26 23:44:26 +0900

    スタックベース仮想マシンの実装をモジュールに整理

--- src/virtual_machine_basics.rs
+++ src/virtual_machine_basics.rs
@@ -2,41 +2,43 @@
 //! 
 //! スタックベース仮想マシン
 
-fn push(stack: &mut Vec<i32>, value: i32) {
-    stack.push(value);
-}
 
-fn add(stack: &mut Vec<i32>) {
-    if stack.len() < 2 {
-        panic!("Stack underflow");
+mod sentence_2_2_impl_rust{
+    fn push(stack: &mut Vec<i32>, value: i32) {
+        stack.push(value);
     }
-    let lhs = stack.pop().unwrap();
-    let rhs = stack.pop().unwrap();
-    stack.push(lhs + rhs);
+    
+    fn add(stack: &mut Vec<i32>) {
+        if stack.len() < 2 {
+            panic!("Stack underflow");
+        }
+        let lhs = stack.pop().unwrap();
+        let rhs = stack.pop().unwrap();
+        stack.push(lhs + rhs);
+    }
+    
+    #[test]
+    fn test_stack_vm() {
+        let mut stack:Vec<i32> = vec![];
+    
+        // 空のスタックを用意する
+        assert_eq!(stack, vec![]);
+    
+        // スタックに値をプッシュする
+        push(&mut stack, 42);
+        assert_eq!(stack, vec![42]);
+    
+        // スタックに値をプッシュする
+        push(&mut stack, 36);
+        assert_eq!(stack, vec![42, 36]);
+    
+        // スタックの値を加算する
+        add(&mut stack);
+        assert_eq!(stack,vec![78]);
+    
+        push(&mut stack, 22);
+        add(&mut stack);
+        assert_eq!(stack, vec![100]);
+    }
 }
-
-#[test]
-fn test_stack_vm() {
-    let mut stack:Vec<i32> = vec![];
-
-    // 空のスタックを用意する
-    assert_eq!(stack, vec![]);
-
-    // スタックに値をプッシュする
-    push(&mut stack, 42);
-    assert_eq!(stack, vec![42]);
-
-    // スタックに値をプッシュする
-    push(&mut stack, 36);
-    assert_eq!(stack, vec![42, 36]);
-
-    // スタックの値を加算する
-    add(&mut stack);
-    assert_eq!(stack,vec![78]);
-
-    push(&mut stack, 22);
-    add(&mut stack);
-    assert_eq!(stack, vec![100]);
-}
-
 
